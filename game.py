6bb004596be4e470964851ef12980374e4043c5e
Ricarth Lima <rrsl@cin.ufpe.br>
2019-09-01 14:16:36 -0300

    adicionado comentarios e alteracoes

--- game.py
+++ game.py
@@ -68,26 +68,37 @@ def pvp_online_servidor(skt):
             skt.sendto("DRAW".encode(),adr)
             break
         else:
-            if vez == 0:
-                
+            # Declara variável de posição da jogada
+            posicao = -1
+
+            # Envia o tabuleiro
+            skt.sendto(str(tabuleiro).encode(),adr)s
+
+            # Caso seja o servidor que vai jogar, posição é definido por input
+            if vez == 0:                
                 print("SUA VEZ")
                 posicao = int(input(">>> "))
+            # Caso seja a vez do cliente, posição é definido por recvfrom
             else:
                 print("Aguardando jogada do adversário...")
                 skt.sendto(str(tabuleiro).encode(),adr)
                 msg,adr = skt.recvfrom(1024)
                 posicao = int(msg.decode())
+
+            # Verificar se a jogada é válida
             if posicao>=0 and posicao <=8 and tabuleiro[posicao]==-1:
                 tabuleiro[posicao] = vez
+                # Se a jogada foi do cliente, avisar a ele que deu tudo certo
+                # enviando o tabuleiro preenchido para ele.
                 if vez == 1:
                     skt.sendto(str(tabuleiro).encode(),adr)
             else:
-                print("Jogada incorreta. Perdeu, pai")
+                #Caso a jogada tenha sido incorreta                
                 if vez == 1:
-                    skt.sendto("JOGADA INCORRETA, PAI".encode(),adr)
+                    print("O cliente é muito noob, errou.")
+                    skt.sendto("MOVE_ERROR".encode(),adr)
                 else:
-                    msg,adr = skt.recvfrom(1024)
-                    print(msg.decode())
+                    print("Jogada incorreta. Perdeu, pai"))
                     
             aux_modules.printarTabuleiro(tabuleiro)
 
@@ -98,11 +109,15 @@ def pvp_online_servidor(skt):
 
                 
 def pvp_online_cliente(skt,servidorIP):
+    # Handshake
     skt.sendto("HANDSHAKE".encode(),(servidorIP,5001))
+    
     while True:
         print("Esperando a vez...")
         msg,adr = skt.recvfrom(1024)
         msg = msg.decode()
+
+        # Caso não receba uma lista, o jogo acabou
         if msg[0]!="[":
             if msg == "WINNER":
                 print("Boa, boy. Tu gera, visse.")
@@ -113,38 +128,37 @@ def pvp_online_cliente(skt,servidorIP):
             else:
                 print("Empatou, comadres")
                 break
+        # Caso receba um tabuleiro, a coisa tem que continuar
         else:
-            msg = msg[1:-1]
-            tabuleiro = msg.split(", ")
-            i = 0
-            while i<len(tabuleiro):
-                tabuleiro[i]=int(tabuleiro[i])
-                i = i+1
-            aux_modules.printarTabuleiro(tabuleiro)
+            # Receber e mostrar o tabuleiro
+            recv_tabuleiro(msg)
+
+            # Receber a jogada do jogador
             print("SUA VEZ")
             posicao = int(input(">>> "))
-            if posicao>=0 and posicao <=8 and tabuleiro[posicao]==-1:
-                skt.sendto(posicao.encode(),(servidorIP,5001))
-                print("Esperando confirmação da jogada...")
-                msg,adr = skt.recvfrom(1024)
-            else:
-                print("Jogada incorreta. Perdeu, pai")
-                if vez == 0:
-                    skt.sendto("Jogou errado.".encode(),(servidorIP,5001))
 
-            if msg.decode == "JOGADA INCORRETA, PAI":
-                print("Adversário errou. Sua vez")
+            # Enviar a jogada
+            skt.sendto(str(posicao).encode(),(servidorIP,5001))
+
+            # Esperar a confirmação da jogada
+            print("Esperando confirmação da jogada...")
+            msg,adr = skt.recvfrom(1024)
+
+            if msg.decode() == "MOVE_ERROR":
+                print("Jogada incorreta, boy.")
             else:
-                msg = msg.decode()
-                msg = msg[1:-1]
-                tabuleiro = msg.split(", ")
-                i = 0
-                while i<len(tabuleiro):
-                    tabuleiro[i]=int(tabuleiro[i])
-                    i = i+1
-                aux_modules.printarTabuleiro(tabuleiro)
+                recv_tabuleiro(msg.decode())
+
+            
         
-        
+def recv_tabuleiro(msg):
+    msg = msg[1:-1]
+    tabuleiro = msg.split(", ")
+    i = 0
+    while i<len(tabuleiro):
+        tabuleiro[i]=int(tabuleiro[i])
+        i = i+1
+    aux_modules.printarTabuleiro(tabuleiro)
 
 
 
